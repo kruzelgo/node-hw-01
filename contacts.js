432df6dcfbc47bfb14e0e70fa88cfaf257306d8a
kruzelgo <kruzelgo@gmail.com>
2024-06-16 18:03:18 +0200

    update

---
+++ contacts.js
@@ -0,0 +1,20 @@
+/*
+ * Skomentuj i zapisz wartość
+ * const contactsPath = ;
+ */
+
+function listContacts() {
+  // ...twój kod
+}
+
+function getContactById(contactId) {
+  // ...twój kod
+}
+
+function removeContact(contactId) {
+  // ...twój kod
+}
+
+function addContact(name, email, phone) {
+  // ...twój kod
+}
